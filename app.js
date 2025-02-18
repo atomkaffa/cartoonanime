03fa7237021b96b6f0ad9b7092153fdb21471d79
atomkaffa <penyouza@gmail.com>
2025-02-18 23:57:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ document.getElementById('convertButton').addEventListener('click', async () => {
 
             // แปลงภาพเป็น Tensor และปรับขนาดเป็น 128x128 ก่อนใส่โมเดล
             let imageTensor = tf.browser.fromPixels(originalImage)
-                .resizeBilinear([512, 512])
+                .resizeBilinear([1024, 1024])
                 .toFloat()
                 .div(127.5)
                 .sub(1)
